85fbb6f61d3e69ebb1e52041b4b6bb16721633c8
igoncor <ireneyuigc@gmail.com>
2024-06-12 19:48:44 +0100

    require de los modelos para comenzar a definir las relaciones

--- BACKEND/database/relations.js
+++ BACKEND/database/relations.js
@@ -1,6 +1,21 @@
 //requerimos los modelos de las tablas
-const User = require('../api/models/user.model')
+const Author = require('../api/models/authors.model')
 const Book = require('../api/models/book.model')
+const User = require('../api/models/user.model')
+const Favorites = require('../api/models/favorites.model')
+const Reviews = require('../api/models/review.model')
+
+const defineRelations = () => {
+    //ONE TO ONE
+
+    //ONE TO MANY
+
+    //MANY TO MANY
+
+
+
+}
+
 
 
 
