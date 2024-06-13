3736f7c88519b0b6f7f210fcb53ec48e6f2e62d0
Migmh92 <migmh.92@gmail.com>
2024-06-13 23:34:33 +0100

    CRUD de authores

--- BACKEND/api/routes/authors.router.js
+++ BACKEND/api/routes/authors.router.js
@@ -3,12 +3,15 @@ const router = require("express").Router()
 const {
   getAllAuthors,
   getOneAuthor,
- 
+  createAuthor,
+  updateAuthor,
+  deleteAuthor
 } = require('../controllers/authors.controller')
 
 
 router.get('/',  getAllAuthors)
 router.get('/:id', getOneAuthor)
-
-
+router.post('/', createAuthor)
+router.put('/:id', updateAuthor)
+router.delete('/:id', deleteAuthor)
 module.exports = router
