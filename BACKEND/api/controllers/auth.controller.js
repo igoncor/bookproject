48a4600997c9bf3a5d608c714ca51c315c3ffd81
Migmh92 <migmh.92@gmail.com>
2024-06-16 22:49:52 +0100

    fix: add kill-process tool onto proyect for debug purposes

--- BACKEND/api/controllers/auth.controller.js
+++ BACKEND/api/controllers/auth.controller.js
@@ -11,11 +11,11 @@ const signup = async (req, res) => {
     const salt = bcrypt.genSaltSync(parseInt(process.env.BCRYPT_SALTS))
     // Encriptamos la contraseña que nos llega por el body de la petición, y la sobreescribimos para luego crear al usuario con la contraseña ya encriptada
     req.body.password = bcrypt.hashSync(req.body.password, salt)
-
+    
     const user = await User.create(req.body)
 
-    // createContact_info es un método generado automáticamente por Sequelize, en el momento de definir una relación One to One entre User y ContactInfo. Podemos aprovecharla para crear automáticamente un nuevo registro de contact info vacío en el momento en el que un nuevo usuario se cree una cuenta, y ya estará automáticamente vinculado al nuevo usuario
-    await user.createContact_info()
+    //createContact_info es un método generado automáticamente por Sequelize, en el momento de definir una relación One to One entre User y ContactInfo. Podemos aprovecharla para crear automáticamente un nuevo registro de contact info vacío en el momento en el que un nuevo usuario se cree una cuenta, y ya estará automáticamente vinculado al nuevo usuario
+    //await user.createContact_info()
 
     // Generamos un token para devolver al cliente, así el usuario ya será capaz de realizar peticiones que reguieran estar logueado
     // {email: user.email} es la información que pasaremos como 'payload'. Cuando desencriptemos el token en el middleware de 'checkAuth', podremos acceder a la info que hayamos definido en este payload.
