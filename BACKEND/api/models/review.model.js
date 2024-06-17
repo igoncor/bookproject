968a23094ffc803f6cb6bf6a0b63b0e2f435d6ba
Migmh92 <migmh.92@gmail.com>
2024-06-17 20:58:19 +0100

    Controladores Favorito y review

--- BACKEND/api/models/review.model.js
+++ BACKEND/api/models/review.model.js
@@ -4,14 +4,7 @@ const { connection } = require('../../database')
 const Review = connection.define(
     "Review",
     {
-        user_id: { 
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        book_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+       
         review: {
             type: DataTypes.TEXT,
             allowNull: false,
