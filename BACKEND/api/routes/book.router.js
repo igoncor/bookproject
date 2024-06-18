2a0e41833cbb8457b4ee99fa3fe1d8a4616dec25
Juanma Sosa <jmasosamendoza@gmail.com>
2024-06-18 17:29:12 +0100

    Update

--- BACKEND/api/routes/book.router.js
+++ BACKEND/api/routes/book.router.js
@@ -16,7 +16,6 @@ const {
 // } = require('../middelwares')
 
 // router.get('/', checkAuth, checkAdmin, getAllBooks)
-<<<<<<< HEAD
 router.get('/',  getAllBooks)
 router.get('/:id', getOneBook)
 router.post('/', createBook)
@@ -26,15 +25,4 @@ router.delete('/:id', deleteBook)
 router.get('/author/:author_id', getBooksByAuthor)
 
 module.exports = router
-=======
-router.get('/',
-  checkAuth,
-  getAllBooks)
-router.get('/:id', 
-  checkAuth,
-  getOneBook)
-// router.post('/', createBook)
-// router.put('/:id', updateOneBook)
-// router.delete('/:id', deleteOneBook)
->>>>>>> 6ef490f83e955400f1bad76f404cc9b874268016
 
