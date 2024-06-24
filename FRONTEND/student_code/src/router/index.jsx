69b7ed93f5afe936715a9daba0a32fd67a1945c8
igoncor <ireneyuigc@gmail.com>
2024-06-24 18:22:44 +0100

    cambios

--- FRONTEND/student_code/src/router/index.jsx
+++ FRONTEND/student_code/src/router/index.jsx
@@ -47,8 +47,8 @@
 import { createBrowserRouter } from "react-router-dom"
 import Root from "../layout/root"
 import Home from "../pages/Home/Home"
-//import SignUpPage from "../pages/SignUp/SignUp"  quitar el comentado cuando se haga merge
-//import SignInPage from "../pages/SignIn/SignIn"  quitar el comentado cuando se haga merge
+import SignUpPage from "../pages/SignUp/SignUp"  
+import SignInPage from "../pages/SignIn/SignIn"  
 import Profile from "../pages/Profile/Profile"
 
 const router = createBrowserRouter([
@@ -57,8 +57,8 @@ const router = createBrowserRouter([
         element: <Root />,
         children: [
             {path: "/home", element: <Home />},
-            //{path: "/sign-up", element: <SignUpPage />},
-            //{path: "/sign-in", element: <SignInPage />},
+            {path: "/sign-up", element: <SignUpPage />},
+            {path: "/sign-in", element: <SignInPage />},
             {path: "/profile", element: <Profile />}
         ]
     },
