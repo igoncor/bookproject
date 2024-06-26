f3dbcb2b28cbb5252aabef5a22d1d3c295c88584
igoncor <ireneyuigc@gmail.com>
2024-06-26 20:08:44 +0100

    estilado

--- FRONTEND/student_code/src/pages/Profile/Profile.jsx
+++ FRONTEND/student_code/src/pages/Profile/Profile.jsx
@@ -43,10 +43,12 @@
 // }
 
 
+
 import "./Profile.css";
 import { useState, useEffect } from 'react';
 import { Avatar, Box, Button, Container, CssBaseline, Grid, Paper, Typography, Link, } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 const Profile = () => {
   const [seconds, setSeconds] = useState(0);
@@ -72,67 +74,94 @@ const Profile = () => {
     }
     return () => clearInterval(interval);
   }, [isActive, seconds]);
+ 
+  
+
 
   return (
     <Container component="main" maxWidth="xl" className="mainContainer">
-      <CssBaseline />
-      <Paper className="paper" elevation={3}>
-        <Avatar className="avatar">
-          <AccountCircleIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Mi perfil
-        </Typography>
-        <Box className="profileDetails">
-          <Typography variant="h6">Pepe Grillo</Typography>
-          <Typography variant="body1">pepito@gmail.com</Typography>
-        </Box>
-        <Button variant="contained" color="primary" className="button">
-          Editar perfil
-        </Button>
-        <Button variant="contained" color="secondary" className="button">
-          Cerrar sesión
-        </Button>
-      </Paper>
-      
-      <Box className="profileContainer">
-        <Box className="favoriteBooks">
-          <Typography variant="h6">Mis libros favoritos</Typography>
-          <Grid container spacing={2} className="gridContainer">
-            {[1, 2, 3, 4].map((book) => (
-              <Grid item key={book} xs={12} sm={6}>
-                <Paper className="bookCard">
-                  <Typography variant="h6">Book Title {book}</Typography>
-                  <Typography variant="body2">Book summary goes here...</Typography>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-
-        <Box className="pendientesBooks">
-          <Typography variant="h6">Mis libros pendientes</Typography>
-          <Grid container spacing={2} className="gridContainer">
-            {[1, 2, 3, 4].map((book) => (
-              <Grid item key={book} xs={12} sm={6}>
-                <Paper className="bookCard">
-                  <Typography variant="h6">Book Title {book}</Typography>
-                  <Typography variant="body2">Book summary goes here...</Typography>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-
+    <CssBaseline />
+    <Paper className="paper" elevation={3}>
+      <Avatar className="avatar">
+        <AccountCircleIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Mi perfil
+      </Typography>
+      <Box className="profileDetails">
+        <Typography variant="h6">Pepe Grillo</Typography>
+        <Typography variant="body1">pepito@gmail.com</Typography>
       </Box>
-      <Box className="comments">
-        <Typography variant="h6">Mis reseñas</Typography>
-        <Typography variant="body2">Comment 1...</Typography>
-        <Typography variant="body2">Comment 2...</Typography>
-        <Typography variant="body2">Comment 3...</Typography>
+      <Button variant="text" className="button">Editar perfil</Button>
+      <Button variant="text" className="button">Cerrar sesión</Button>
+    </Paper>
+
+    <Box className="profileContainer">
+      <Box className="favoriteBooks">
+        <Typography variant="h6">Mis libros favoritos</Typography>
+        <Grid container spacing={2} className="gridContainer">
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">El Símbolo Perdido</Typography>
+             
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Tormenta de Espadas</Typography>
+              
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Choque de Reyes</Typography>
+              
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Juego de Tronos</Typography>
+              
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
-    </Container>
-  );
+
+      <Box className="pendientesBooks">
+        <Typography variant="h6">Mis libros pendientes</Typography>
+        <Grid container spacing={2} className="gridContainer">
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Origen</Typography>
+              
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Inferno</Typography>
+              
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Ángeles y Demonios</Typography>
+              
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className="bookCard">
+              <Typography variant="h6">Festín de Cuervos</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
+
+    </Box>
+
+    
+  </Container>
+);
 };
 
-export default Profile;;
+export default Profile;
+
