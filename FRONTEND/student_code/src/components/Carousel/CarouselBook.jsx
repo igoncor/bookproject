bb772d26fcb546cb3547eb65777b7063093592e8
Juanma Sosa <jmasosamendoza@gmail.com>
2024-06-27 09:51:43 +0100

    Carousel Home

--- FRONTEND/student_code/src/components/Carousel/CarouselBook.jsx
+++ FRONTEND/student_code/src/components/Carousel/CarouselBook.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-// import SwipeableViews from 'react-swipeable-views';
-// import { autoPlay } from 'react-swipeable-views-utils';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import Carousel from 'react-material-ui-carousel'
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './CarouselBook.css';
 
 import carouselImg1 from '../../assets/img/carousel-img1.jpg';
@@ -19,119 +10,41 @@ import carouselImg3 from '../../assets/img/carousel-img3.jpg';
 import carouselImg4 from '../../assets/img/carousel-img4.jpg';
 import carouselImg5 from '../../assets/img/carousel-img5.jpg';
 
-// const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
 const images = [
-  {
-    label: '',
-    imgPath: carouselImg1,
-  },
-  {
-    label: '',
-    imgPath: carouselImg2,
-  },
-  {
-    label: '',
-    imgPath: carouselImg3,
-  },
-  {
-    label: '',
-    imgPath: carouselImg4,
-  },
-  {
-    label: '',
-    imgPath: carouselImg5,
-  },
+  { src: carouselImg1, alt: 'Carousel Image 1' },
+  { src: carouselImg2, alt: 'Carousel Image 2' },
+  { src: carouselImg3, alt: 'Carousel Image 3' },
+  { src: carouselImg4, alt: 'Carousel Image 4' },
+  { src: carouselImg5, alt: 'Carousel Image 5' }
 ];
 
-function CarouselBooks() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
+function Carousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+    <div className="carousel-background">
+      <div className="carousel-container">
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index} className="carousel-slide">
+              <img src={image.src} alt={image.alt} className="carousel-image" />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
   );
 }
 
-export default CarouselBooks;
+export default Carousel;
+
+
--- FRONTEND/student_code/src/pages/Home/Home.jsx
+++ FRONTEND/student_code/src/pages/Home/Home.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import BookCard from '../../components/BookCard/BookCard';
 import { getAllBooks } from '../../services/titleService.js';
-
+import Carousel from '../../components/Carousel/CarouselBook'
 import Rank from '../../components/Rank/Rank'
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Home.css'
-// import CarouselBook from '../../components/Carousel/CarouselBook.jsx'
+
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
@@ -17,7 +20,7 @@ const BookList = () => {
       try {
         const allBooks = await getAllBooks(); //Cogemos todos los libros
         // Filtrar los libros por las IDs específicas
-        const filteredBooks = allBooks.filter(book => [4, 2, 3, 7, 11, 20].includes(book.id));
+        const filteredBooks = allBooks.filter(book => [12, 13, 10, 11, 17].includes(book.id));
         setBooks(filteredBooks);
       } catch (error) {
         setError(error);
@@ -30,32 +33,20 @@ const BookList = () => {
   }, []);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return <p>Error: {error.message}</p>;  
 
-   const topBooks = books.filter(book => book.likes === 5);
-  // console.log('Número de libros con likes=5:', topBooks.length);
-// console.log (books);
   return (    
   <div>
     {/* <CarouselBook /> */}
-        
-      <p>You are in Home</p>
+      <Carousel />  
+      {/* <p>You are in Home</p> */}
     <div className="read-this">
       <h2>Qué leer:</h2>
       <div className="book-grid">
         {books.map(book => (
           <BookCard key={book.id} book={book} />
         ))}
-      </div> 
-           
-         {/* <div className="top-rank" style={{ border: '2px solid #f7b718', padding: '20px', borderRadius: '10px', boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)', marginBottom: '20px', maxHeight: '400px', overflowY: 'auto' }}>
-        <h2 style={{ color: '#f7b718', marginBottom: '15px' }}>Top-5:</h2>
-        <ol style={{ textAlign: 'left', color: '#ffffff' }}>
-          {topBooks.map((book) => (
-            <li key={book.id}>{book.title} by {book.author.name} {book.author.surname}</li>
-          ))}
-        </ol>
-      </div> */}
+      </div>                 
       <div className= "top-rank">
         <Rank />
       </div>
